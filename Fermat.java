d31b81f3c26985506ae2dcf5192481c32daf6739
Shane Hare <sxh4645@rit.edu>
2016-03-31 23:33:44 -0400

    Complete Implementation

--- Fermat.java
+++ Fermat.java
@@ -1,30 +1,45 @@
 import edu.rit.pj2.Loop;
 import edu.rit.pj2.Task;
+import edu.rit.pj2.vbl.IntVbl;
+import java.math.BigInteger;
 
 public class Fermat extends Task{
+	
+	IntVbl counter = new IntVbl.Sum();
+	int number;
 	
 	//Main must throw exception and NOT implement System.Exit(1);
 	public void main(String[] args) throws Exception{
 		if (args.length < 1) usage();
 		
-		int number = Integer.parseInt(args[0]);
+		number = Integer.parseInt(args[0]);
 		
 		parallelFor (0, number - 1) .exec (new Loop()
 		{
+			IntVbl thrCounter;
+			BigInteger a;
+			BigInteger p;
+			
             public void start()
 			{
-                 // One-time thread-local initialization (optional method)
+				thrCounter = threadLocal (counter);
+				
+                // One-time thread-local initialization (optional method)
             }
             public void run (int i)
             {
-                 // Loop body code for iteration i (required method)
+				a = new BigInteger(Integer.toString(i));
+				p = new BigInteger(Integer.toString(number));
+				if (a.modPow(p, p).compareTo(a) != 0)
+				{
+					// a is a witness
+					++ thrCounter.item;
+				}				
+                // Loop body code for iteration i (required method)
             }
-             public void finish()
-            {
-                 // One-time thread-local finalization (optional method)
-            }
-
 		});
+		
+		System.out.printf ("Counter = %d%n", counter.item);
 	}
 	
 	public void usage() throws Exception{
